2d478a5db1a6b2ea1aec8ee5995970a85ebe1c2f
vamshikrishnakyatham <vamshikrishna.kyatham.22@gmail.com>
2022-05-02 14:14:53 +0530

    Assignment_

--- src/Assignment11.java
+++ src/Assignment11.java
@@ -4,31 +4,35 @@ import java.util.Map;
 
 public class Assignment11 {
     public static void main(String[] args) throws IOException {
-        Map<String, Integer> charCounter = new HashMap<>();
-        FileInputStream file = new FileInputStream("/Users/Vamshi Krishna/sessions");
-        DataInputStream dis = new DataInputStream(file);
-        BufferedReader br = new BufferedReader(new InputStreamReader(dis));
-        FileOutputStream fileOut=new FileOutputStream("/Users/Vamshi Krishna/output.txt");
-        ObjectOutputStream out=new ObjectOutputStream(fileOut);
-        String temp = "";
-        while ((temp=br.readLine()) != null)
-        {
-            String[] spliter= temp.split("");
-            for(String temp1:spliter)
-                if(charCounter.containsKey(temp1)){
-                    charCounter.put(temp1,charCounter.get(temp1)+1);
-                }else{
-                    charCounter.put(temp1,1);
-                }
+        try {
+            Map<String, Integer> charCounter = new HashMap<>();
+            FileInputStream file = new FileInputStream("sessions");
+            DataInputStream dis = new DataInputStream(file);
+            BufferedReader br = new BufferedReader(new InputStreamReader(dis));
+            FileOutputStream fileOut = new FileOutputStream("output.txt");
+            ObjectOutputStream out = new ObjectOutputStream(fileOut);
+            String temp = "";
+            while ((temp = br.readLine()) != null) {
+                String[] spliter = temp.split("");
+                for (String temp1 : spliter)
+                    if (charCounter.containsKey(temp1)) {
+                        charCounter.put(temp1, charCounter.get(temp1) + 1);
+                    } else {
+                        charCounter.put(temp1, 1);
+                    }
 
 
+            }
+
+            out.writeObject(charCounter.toString());
+            out.close();
+            fileOut.close();
+            System.out.println("file saved.");
+            System.out.println(charCounter);
         }
-
-        out.writeObject(charCounter);
-        out.close();
-        fileOut.close();
-        System.out.println("file saved.");
-        System.out.println(charCounter);
+        catch(FileNotFoundException ex){
+            System.out.println("File not found!");
+        }
 
     }
 }
